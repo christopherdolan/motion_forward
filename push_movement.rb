a6d894fcde1a44945c38b0cd522be0baa2def143
Chris Dolan <aelien27@gmail.com>
2015-04-01 12:57:23 -0700

    Better logging.

--- push_movement.rb
+++ push_movement.rb
@@ -4,18 +4,21 @@ require 'pushbullet'
 
 api_key_file = File.join(File.dirname(__FILE__), 'pushbullet_api.key')
 
+puts "Setting access token."
 if File.readable? api_key_file
 	Pushbullet.set_access_token(File.read(api_key_file))
+	puts "Access token set."
 else
-	STDERR.puts('Keyfile not found: %s', api_key_file)
+	STDERR.puts("Keyfile not found: #{api_key_file}")
 	exit -2
 end
 
-event, filename = ARGV
-
+filename = ARGV.first
+puts "Filename: #{filename}"
 if File.readable? filename
 	Pushbullet::V2::Push.file(filename, "Motion: front door")
+	puts "File #{filename} pushed to all devices."
 else
-	STDERR.puts('File not found: %s', filename)
+	STDERR.puts("File not found: #{filename}")
 	exit -1
 end
