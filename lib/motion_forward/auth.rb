6a05ec07f23d5bbd19a9f8a94e640752f7b2f3eb
Chris Dolan <aelien27@gmail.com>
2015-04-02 15:57:48 -0700

    Fail fast if no auth found

--- lib/motion_forward/auth.rb
+++ lib/motion_forward/auth.rb
@@ -3,7 +3,10 @@ def auth
 	if File.readable?(@options[:key])
 		puts "Reading key from file at #{@options[:key]}"
 		@options[:key] = File.read @options[:key]
+		Pushbullet.set_access_token @options[:key]
+		puts "Access token set."
+	else
+		STDERR.puts "File not accessbile: #{@options[:key]}"
+		exit -1
 	end
-	Pushbullet.set_access_token @options[:key]
-	puts "Access token set."
 end
